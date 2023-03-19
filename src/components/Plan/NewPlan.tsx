86da45015bcf12627e60ce448bbdcd34f062558b
Arsh Khatwani <arsh.khatwani@gmail.com>
2023-03-19 14:21:05 +0530

    Add plan form

--- src/components/Plan/NewPlan.tsx
+++ src/components/Plan/NewPlan.tsx
@@ -1,11 +1,120 @@
-import { View, Text } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import React from 'react';
+import tw from 'twrnc';
+import { HelperText, Text, TextInput } from 'react-native-paper';
+import BackBtn from '../Buttons/BackBtn';
+import styles from '../../styles/NewTransaction';
+import PrimaryBtn from '../Buttons/PrimaryBtn';
+import Dropdown from '../Transaction/Dropdown';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import planCategories from '../../constants/planCategories';
+
+export interface PlanFormFields {
+  name: string;
+  category: string;
+  target: string;
+}
+
+const categories = planCategories.map(item => ({
+  label: item,
+  value: item,
+}));
+
+const validationSchema = yup.object().shape({
+  name: yup.string().min(2, 'Too short').required('Field is required'),
+  target: yup
+    .string()
+    .required('Field is required')
+    .test('is-number', 'Field must be a valid number', value => {
+      const number = Number(value);
+      if (Number.isNaN(number)) {
+        return false;
+      }
+      return true;
+    }),
+});
+
+const initialValues: PlanFormFields = {
+  name: '',
+  category: categories[0].value,
+  target: '',
+};
 
 const NewPlan = () => {
   return (
-    <View>
-      <Text>NewPlan</Text>
-    </View>
+    <ScrollView>
+      <View style={tw`px-5 pt-5`}>
+        <View style={tw`flex flex-row items-center mb-7`}>
+          <BackBtn style={tw`mr-1.5`} />
+          <Text style={[styles.heading, tw`pb-1`]}>New Plan</Text>
+        </View>
+
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={(values: any) => {
+            console.log(values);
+          }}>
+          {({
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            values,
+            errors,
+            setValues,
+          }) => (
+            <>
+              <View style={tw`mb-6`}>
+                <Text style={[styles.formLabel]}>Plan Name</Text>
+                <TextInput
+                  mode="outlined"
+                  placeholder="Buy vehicle, house, travel etc."
+                  onChangeText={handleChange('name')}
+                  onBlur={handleBlur('name')}
+                  value={values.name}
+                />
+                {errors.name && (
+                  <HelperText type="error">{errors.name}</HelperText>
+                )}
+              </View>
+
+              <View style={tw`mb-6`}>
+                <Text style={[styles.formLabel]}>Target</Text>
+                <TextInput
+                  mode="outlined"
+                  placeholder="in Rs."
+                  onChangeText={handleChange('target')}
+                  onBlur={handleBlur('target')}
+                  value={values.target}
+                  keyboardType="number-pad"
+                />
+                {errors.target && (
+                  <HelperText type="error">{errors.target}</HelperText>
+                )}
+              </View>
+
+              <View style={tw`mb-6`}>
+                <Text style={[tw`mb-2`, styles.formLabel]}>Category</Text>
+                <Dropdown
+                  values={values}
+                  setValues={setValues}
+                  categories={categories}
+                />
+              </View>
+
+              <View>
+                <PrimaryBtn
+                  label="Save"
+                  onPress={handleSubmit}
+                  style={tw`my-3`}
+                />
+              </View>
+            </>
+          )}
+        </Formik>
+      </View>
+    </ScrollView>
   );
 };
 
---
+++ src/constants/planCategories.ts
@@ -0,0 +1,11 @@
+const categories = [
+  'Travel or Vacation',
+  'Vehicle',
+  'House or Real Estate',
+  'Retirement',
+  'Education',
+  'Business or Investment',
+  'Emergency Fund',
+];
+
+export default categories;

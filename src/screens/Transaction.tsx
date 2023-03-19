a19853e55ff700e38d8c1233a1e117af94bae510
Arsh Khatwani <arsh.khatwani@gmail.com>
2023-03-19 10:15:53 +0530

    Add scrollview in transaction screen

--- src/screens/Transaction.tsx
+++ src/screens/Transaction.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import tw from 'twrnc';
 import TransactionHeader from '../components/Transaction/TransactionHeader';
 import UserTransactions from '../components/Transaction/UserTransactions';
 
 const Transaction = () => {
   return (
-    <View>
+    <ScrollView stickyHeaderIndices={[0]}>
       <TransactionHeader />
 
       <View style={tw`px-5`}>
         <UserTransactions />
       </View>
-    </View>
+    </ScrollView>
   );
 };
 

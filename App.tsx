ae09b9b1ffd41c2eb1894aeb781c22ecc1820014
Arsh Khatwani <arsh.khatwani@gmail.com>
2023-04-01 19:52:24 +0530

    Add swipe feature and delete btn in budget card

--- App.tsx
+++ App.tsx
@@ -6,18 +6,21 @@ import RootNavigation from './src/navigations/RootNavigation';
 import store from './src/redux/store';
 import { Provider as PaperProvider } from 'react-native-paper';
 import theme from './src/constants/appTheme';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 function App(): JSX.Element {
   return (
-    <Provider store={store}>
-      <PaperProvider theme={theme}>
-        <NavigationContainer>
-          <SafeAreaView style={{ flex: 1 }}>
-            <RootNavigation />
-          </SafeAreaView>
-        </NavigationContainer>
-      </PaperProvider>
-    </Provider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Provider store={store}>
+        <PaperProvider theme={theme}>
+          <NavigationContainer>
+            <SafeAreaView style={{ flex: 1 }}>
+              <RootNavigation />
+            </SafeAreaView>
+          </NavigationContainer>
+        </PaperProvider>
+      </Provider>
+    </GestureHandlerRootView>
   );
 }
 
--- src/components/Budget/BudgetCard.tsx
+++ src/components/Budget/BudgetCard.tsx
@@ -1,18 +1,32 @@
 import React, { FC } from 'react';
 import { Image, View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { IconButton, Text, MD2Colors } from 'react-native-paper';
 import { useSelector } from 'react-redux';
 import tw from 'twrnc';
 import { categoriesIcons } from '../../constants/transactionCategories';
 import { RootState } from '../../redux/store';
 import styles from '../../styles/PlanCard';
 import ProgressBar from '../ProgressBar';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 interface Props {
   category: string;
   target: number;
   bid: string;
 }
+
+const Options: FC = () => {
+  return (
+    <View style={tw`flex-col items-center justify-center`}>
+      <IconButton
+        icon="delete"
+        size={25}
+        onPress={() => {}}
+        iconColor={MD2Colors.red400}
+      />
+    </View>
+  );
+};
 
 const BudgetCard: FC<Props> = ({ category, target }) => {
   const { categoryTransactions } = useSelector(
@@ -24,26 +38,31 @@ const BudgetCard: FC<Props> = ({ category, target }) => {
   const spend = categoryTransactions[category];
 
   return (
-    <View style={[tw`px-3 py-3 my-2`, styles.container]}>
-      <View style={tw`flex flex-row items-center mb-3`}>
-        <View style={[styles.icon, tw`mr-2`]}>
-          <Image style={styles.imageIcon} source={categoryIcon} />
+    <Swipeable
+      renderLeftActions={() => <></>}
+      containerStyle={[tw`px-3 py-3 my-2`, styles.container]}
+      renderRightActions={Options}>
+      <View>
+        <View style={tw`flex flex-row items-center mb-3`}>
+          <View style={[styles.icon, tw`mr-2`]}>
+            <Image style={styles.imageIcon} source={categoryIcon} />
+          </View>
+
+          <View>
+            <Text style={styles.heading}>{category}</Text>
+          </View>
         </View>
 
-        <View>
-          <Text style={styles.heading}>{category}</Text>
+        <View style={tw`flex flex-row items-center justify-between mb-3`}>
+          <Text style={styles.currentSaving}>Budget ₹ {target}</Text>
+          <Text style={styles.targetAmt}>Remaining ₹ {target - spend}</Text>
+        </View>
+
+        <View style={tw`mb-3`}>
+          <ProgressBar curVal={spend} maxVal={target} />
         </View>
       </View>
-
-      <View style={tw`flex flex-row items-center justify-between mb-3`}>
-        <Text style={styles.currentSaving}>Budget ₹ {target}</Text>
-        <Text style={styles.targetAmt}>Remaining ₹ {target - spend}</Text>
-      </View>
-
-      <View style={tw`mb-3`}>
-        <ProgressBar curVal={spend} maxVal={target} />
-      </View>
-    </View>
+    </Swipeable>
   );
 };
 
